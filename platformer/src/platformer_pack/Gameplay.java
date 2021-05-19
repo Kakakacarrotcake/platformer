40c7130addf3fc2f0f906e96f34c2e35e96910a0
necko <necko@mail.com>
2021-05-19 13:11:25 +0200

    first commit

---
+++ platformer/src/platformer_pack/Gameplay.java
@@ -0,0 +1,36 @@
+package platformer_pack;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JPanel;
+
+public class Gameplay extends JPanel implements KeyListener, ActionListener{
+
+	
+	public Gameplay() {
+		
+	}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {}
+	@Override
+	public void keyTyped(KeyEvent e) {}
+
+
+}
---
+++ platformer/src/platformer_pack/Platformer.java
@@ -0,0 +1,19 @@
+package platformer_pack;
+
+import javax.swing.JFrame;
+
+public class Platformer {
+
+	public static void main(String[] args) {
+		
+		
+		JFrame frame = new JFrame();
+		frame.setBounds(50, 50, 1000, 800);
+		frame.setTitle("Platformer");
+		frame.setVisible(true);
+		frame.setResizable(false);
+		//frame.add
+		
+	}
+
+}
